a1065a242d1c7e974cab9cdb411bc18835997f95
cmshelplive <cmshelplive@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-21 10:43:08 +0000

    version update
    git-svn-id: https://plugins.svn.wordpress.org/custom-user-contact-form-builder/trunk@1140474 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 	Plugin Name: Contact Form Pro
 	Plugin URI: https://wordpress.org/plugins/custom-user-contact-form-builder/
 	Description: An easy to use, simple but powerful contact form system that also tracks submissions through a nifty interface. You can create unlimited forms with custom fields and use them through WordPress shortcode system.
-	Version: 2.1.0
+	Version: 2.1.1
 	Author: CMSHelpLive
 	Author URI: https://profiles.wordpress.org/cmshelplive
 	License: gpl2
